9cf515d0e6877566d346ed577d89e031aabc8c05
Lucas Labre <lucas.melo@mjv.com.br>
2023-07-21 21:20:04 -0300

    Criação do projeto spring

---
+++ aulas_particulares_spring/src/main/java/com/aulas_particulares/AulasParticularesApplication.java
@@ -0,0 +1,13 @@
+package com.aulas_particulares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulasParticularesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulasParticularesApplication.class, args);
+	}
+
+}
---
+++ aulas_particulares_spring/src/test/java/com/aulas_particulares/AulasParticularesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aulas_particulares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulasParticularesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
